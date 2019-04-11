cf3b76ecf22c290bfd7051a767d7868a2b46d274
tuwang <1319177784@qq.com>
2019-04-11 21:41:51 +0800

    轮播图部分的组件

---
+++ src/components/Bananer.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="swiper-container billboards">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide" v-for="(item, index) in banners" :key="index">
+        <img :src="item.imgUrl" alt>
+      </div>
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    banners: {
+      type: Array
+    }
+  },
+  data() {
+    return{
+      swiper: null
+    }
+  },
+  updated() {
+    if (!this.swiper) {
+      new Swiper(".swiper-container", {
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination"
+        }
+      })
+    }
+  }
+};
+</script>
+
+<style lang="less">
+.swiper-container {
+  width: 100%;
+  height: 210px;
+  img {
+    width: 100%;
+  }
+}
+</style>
--- src/views/Films.vue
+++ src/views/Films.vue
@@ -1,5 +1,41 @@
 <template>
   <div>
-    电影中心
+    <Bananer :banners="bannerList"></Bananer>
   </div>
 </template>
+
+<script>
+import Bananer from '../components/Bananer';
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      bannerList: []
+    }
+  },
+  components: {
+    Bananer
+  },
+  methods: {
+    getBannerList() {
+      axios.get('https://m.maizuo.com/gateway?type=2&cityId=440300&k=3684551', {
+        headers: {
+          'X-Client-Info': '{"a":"3000","ch":"1002","v":"1.0.0","e":"15546272691911260447363"}',
+          'X-Host': 'mall.cfg.common-banner'
+        }
+      }).then(res => {
+        let data = res.data;
+        if(data.status === 0){
+          this.bannerList = data.data;
+        }else {
+          alert(data.msg);
+        }
+      })
+    }
+  },
+
+  created() {
+    this.getBannerList();
+  }
+}
+</script>
